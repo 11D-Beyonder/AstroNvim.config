94f83e4a89d354c846cd062f869da00bef779ec7
11D-Beyonder <ztz20001117@126.com>
2024-02-15 21:56:33 +0800

    python支持
    1. <leader>lv 选择虚拟环境
    2. 引入社区包，单独设置环境选择插件
    3. 支持代码编写和调试

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,7 +4,20 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
+  { import = "astrocommunity.pack.json" },
+
   { import = "astrocommunity.pack.lua" },
+
+  -- python支持
+  { import = "astrocommunity.pack.python" },
+  {
+    "linux-cultist/venv-selector.nvim",
+    ft = "python",
+    opts = {
+      anaconda_base_path = "~/miniconda3",
+      anaconda_envs_path = "~/miniconda3/envs",
+    },
+  },
 
   { import = "astrocommunity.debugging.nvim-dap-virtual-text" },
 
