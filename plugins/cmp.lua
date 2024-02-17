37d9c3c206ced5e486dd9038555b62c881c54408
11D-Beyonder <ztz20001117@126.com>
2024-02-17 19:48:58 +0800

    cmp：添加cmdline
    1. Tab&S-Tab切换
    2. 命令和查找补全

--- plugins/cmp.lua
+++ plugins/cmp.lua
@@ -1,9 +1,15 @@
 return {
   "hrsh7th/nvim-cmp",
+  dependencies = {
+    {
+      "hrsh7th/cmp-cmdline",
+      config = require "user.plugins.configs.cmp-cmdline",
+    },
+  },
   opts = function(_, opts)
     return require("astronvim.utils").extend_tbl(opts, {
       completion = {
-        completeopt = "menu,menuone,noinsert",
+        completeopt = "menu,menuone,noinsert,preview",
       },
       formatting = {
         format = require("lspkind").cmp_format {
@@ -13,6 +19,7 @@ return {
             path = "[Path]",
             nvim_lsp = "[LSP]",
             luasnip = "[LuaSnip]",
+            cmdline = "[Cmd]",
           },
         },
       },
