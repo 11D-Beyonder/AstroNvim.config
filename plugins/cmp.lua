b8885e94859e01aff11aa7d3e75cb144d938fdf9
11D-Beyonder <ztz20001117@126.com>
2024-02-17 20:04:26 +0800

    cmp：添加Latex符号补全
    补全公式，确定后插入字符

--- plugins/cmp.lua
+++ plugins/cmp.lua
@@ -5,6 +5,7 @@ return {
       "hrsh7th/cmp-cmdline",
       config = require "user.plugins.configs.cmp-cmdline",
     },
+    "kdheepak/cmp-latex-symbols",
   },
   opts = function(_, opts)
     return require("astronvim.utils").extend_tbl(opts, {
@@ -22,6 +23,13 @@ return {
             cmdline = "[Cmd]",
           },
         },
+      },
+      sources = require("cmp").config.sources {
+        { name = "nvim_lsp",      priority = 9 },
+        { name = "luasnip",       priority = 8 },
+        { name = "buffer",        priority = 7 },
+        { name = "path",          priority = 6 },
+        { name = "latex_symbols", priority = 5, option = { strategy = 0 } },
       },
     })
   end,
